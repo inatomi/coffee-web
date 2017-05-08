d110a0f8ca1d7a81e660b7870bacfea80adae864
inatomi <inatomi@itrade.co.jp>
2017-05-08 11:07:42 +0900

    fixxed

--- js/cognito.js
+++ js/cognito.js
@@ -1,11 +1,11 @@
 // Initialize the Amazon SDK
-AWS.config.region = 'ap-northeast-1'; // Region
+AWS.config.region = 'ap-northeast-1';
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
   IdentityPoolId: 'ap-northeast-1:X',
 });
 
 // Initialize the Amazon Cognito credentials provider
-AWSCognito.config.region = 'ap-northeast-1'; // Region
+AWSCognito.config.region = 'ap-northeast-1';
 AWSCognito.config.credentials = new AWS.CognitoIdentityCredentials({
   IdentityPoolId: 'ap-northeast-1:X'
 });
